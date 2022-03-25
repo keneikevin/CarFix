8f94d609c38b5d0c2d524b94a08c64399e52dd7d
keneikevin <kkevinkenei@gmail.com>
2022-03-25 21:02:18 +0300

    final

--- app/src/main/java/com/example/allscreensizes/MainActivity.kt
+++ app/src/main/java/com/example/allscreensizes/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.allscreensizes
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +9,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.setDisplayShowHomeEnabled(true);
+        supportActionBar?.setLogo(R.drawable.bar);
+        supportActionBar?.setDisplayUseLogoEnabled(true);
+        supportActionBar?.setBackgroundDrawable(
+            ColorDrawable(
+            Color.parseColor("#FF3700B3")
+        )
+        );
     }
 }
